49c03d88c904467be93f6afc8bab485f9cf9157c
sushil kumar <sushilkumar@sushils-MacBook-Air.local>
2019-07-28 16:09:42 +0530

    defined source in params.
    Signed-off-by: sushil kumar <sushilkumar@sushils-MacBook-Air.local>

--- repository.js
+++ repository.js
@@ -2,8 +2,10 @@ const { getHeaders } = require('./util');
 
 export const  navigationApi = {
   method: 'get',
-  url: GlobalConfig.getApiUrlFromRoot('/getNavigation.php'),
+  url: '/getNavigation.php',
   headers: getHeaders(['JSON', 'SERVICE_WORKER_CACHED']), // this api response will be cached by service worker
-  params: {},
+  params: { // default params for api.
+    source: 'web',
+  },
   dispatchAs: 'UPDATE_NAVIGATION'
 }
